0e9d33514a694a69c8f7679326c437c0fe36c4c5
m4nu56 <m4nu56@users.noreply.github.com>
2020-05-05 12:44:14 +0000

    Automated deployment: Tue May  5 12:44:14 UTC 2020 5e45d087a9fba0950aab7e484f3c38c218b01f65

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.d669f0ddc3607c668b7b140eb0ff223e.js"
+  "precache-manifest.9851f5d2fdfc2b84197b9a2ee6b06289.js"
 );
 
 self.addEventListener('message', (event) => {
