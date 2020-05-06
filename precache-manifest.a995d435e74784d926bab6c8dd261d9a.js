bcc8b76cd64abcd324b3b1c48bedd8d184d76287
m4nu56 <m4nu56@users.noreply.github.com>
2020-05-06 03:17:28 +0000

    Automated deployment: Wed May  6 03:17:28 UTC 2020 43909e9594faada7b12fa0f0949c467618227887

---
+++ precache-manifest.a995d435e74784d926bab6c8dd261d9a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d0aa347702d70fe0085f0813b6f37cc",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.5eba3e8f6210cf9306bc.bundle.js"
+  },
+  {
+    "url": "runtime~main.5eba3e8f6210cf9306bc.bundle.js"
+  },
+  {
+    "url": "vendors~main.5eba3e8f6210cf9306bc.bundle.js"
+  },
+  {
+    "url": "vendors~main.5eba3e8f6210cf9306bc.bundle.js.LICENSE.txt"
+  }
+]);
